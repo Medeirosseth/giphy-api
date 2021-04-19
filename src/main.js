4db4eae123ee4ad91f26ebf68568ad0bf8f940f7
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-04-19 15:05:55 -0700

    add giphy random api
    Co-authored-by: Nick Reeder <nickreeder32@gmail.com>

--- src/main.js
+++ src/main.js
@@ -3,9 +3,31 @@ import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
 
+// Utility functions
+function getElements(response) {
+  if (Array.isArray(response.data)) {
+    $(".giphy").append(
+      '<img id="gif" src="' + response.data[0].images.downsized_large.url + '">'
+    );
+  } else {
+    $(".giphy").append(
+      '<img id="gif" src="' + response.data.images.downsized_large.url + '">'
+    );
+    if (response.data.title) {
+      $("#search").val(response.data.title);
+    }
+  }
+}
+
+function clearPastGif() {
+  $("#gif").remove();
+}
+
+// UI Logic
 $("#submitButton").on("click", (event) => {
   event.stopImmediatePropagation();
   event.preventDefault();
+  clearPastGif();
   const searchString = $("#search").val();
 
   let request = new XMLHttpRequest();
@@ -20,11 +42,19 @@ $("#submitButton").on("click", (event) => {
 
   request.open("GET", url, true);
   request.send();
+});
 
-  function getElements(response) {
-    // we need to get image url from images
-    $(".giphy").append(
-      '<img src="' + response.data[0].images.downsized_large.url + '">'
-    );
-  }
+$("#random").on("click", () => {
+  const url = `http://api.giphy.com/v1/gifs/random?api_key=${process.env.API_KEY}`;
+  let request = new XMLHttpRequest();
+  clearPastGif();
+  request.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+      const response = JSON.parse(this.responseText);
+      console.log(response);
+      getElements(response);
+    }
+  };
+  request.open("GET", url, true);
+  request.send();
 });
