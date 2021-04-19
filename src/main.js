af97ad91b118290338af911fdd5a5aa1270a0ea7
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-04-19 14:24:39 -0700

    add giphy API
    Co-authored-by: Nick Reeder <nickreeder32@gmail.com>

--- src/main.js
+++ src/main.js
@@ -1,9 +1,30 @@
+import $ from "jquery";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./css/styles.css";
 
-import $ from 'jquery';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/styles.css';
+$("#submitButton").on("click", (event) => {
+  event.stopImmediatePropagation();
+  event.preventDefault();
+  const searchString = $("#search").val();
 
-$.ready(() => {
+  let request = new XMLHttpRequest();
+  const url = `http://api.giphy.com/v1/gifs/search?q=${searchString}&limit=1&api_key=${process.env.API_KEY}`;
 
+  request.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+      const response = JSON.parse(this.responseText);
+      getElements(response);
+    }
+  };
+
+  request.open("GET", url, true);
+  request.send();
+
+  function getElements(response) {
+    // we need to get image url from images
+    $(".giphy").append(
+      '<img src="' + response.data[0].images.downsized_large.url + '">'
+    );
+  }
 });
